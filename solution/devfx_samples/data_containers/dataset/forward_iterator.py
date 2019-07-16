083d464fa875b63884e23631768364234bdd99ad
vasilegrafu <vasilegrafu@gmail.com>
2019-07-16 21:56:24 +0300

    Add iterator parameter in sample

--- solution/devfx_samples/data_containers/dataset/forward_iterator.py
+++ solution/devfx_samples/data_containers/dataset/forward_iterator.py
@@ -5,7 +5,7 @@ v = [np.arange(0, 10, 1), 2*np.arange(0, 10, 1)]
 print(v)
 
 ds = dc.Dataset(v)
-ds_iterator = ds.iterator(4)
+ds_iterator = ds.iterator(batch_size=4)
 
 ds_iterator_data = ds_iterator.next()
 print(ds_iterator_data)
