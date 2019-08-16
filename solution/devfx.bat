bce68c455c406b348b6f38e5a679fa5400c989ac
vasilegrafu <vasilegrafu@gmail.com>
2019-08-16 14:21:17 +0300

    Switched to VSCode-win32-x64-1.37.1

--- solution/devfx.bat
+++ solution/devfx.bat
@@ -4,4 +4,4 @@ SET PATH=i:\Installations\Miniconda3-4.5.4-x86_64\envs\devfx;%PATH%
 SET QT_PLUGIN_PATH=i:\Installations\Miniconda3-4.5.4-x86_64\envs\devfx\Library\plugins
 SET PYTHONPATH=i:\Dev.Work\devfx.python\solution;%PYTHONPATH%
 
-i:\Installations\VSCode-win32-x64-1.28.2\Code.exe devfx.code-workspace
+i:\Installations\VSCode-win32-x64-1.37.1\Code.exe devfx.code-workspace
