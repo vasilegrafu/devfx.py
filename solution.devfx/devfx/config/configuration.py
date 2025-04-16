69d102e51167b8c86232d7745dd1774fd4f8a134
Vasile Grafu <vasilegrafu@gmail.com>
2025-04-16 20:34:02 +0300

    .

--- solution.devfx/devfx/config/configuration.py
+++ solution.devfx/devfx/config/configuration.py
@@ -1,17 +1,34 @@
 import json as json
 from ..core import GlobalStorage
 from ..core import is_instance
+from ..exceptions import NotFoundError
 
 class Configuration(object):
     def __init__(self, config_node):
         self.__config_node = config_node
 
-    def __getitem__(self, key):
-        config_node_value = self.__config_node[key]
-        if(is_instance(config_node_value, dict)):
-            return Configuration(config_node_value)
+    def __getitem__(self, key):    
+        config_node = self.__config_node    
+        if ':' in key:
+            keys = key.split(':')
+            for k in keys:
+                if(k in config_node):
+                    config_node = config_node[k]
+                else:
+                    raise NotFoundError(f"Key '{key}' not found in configuration.")
+                
+            if(is_instance(config_node, dict)):
+                return Configuration(config_node)
+            else:
+                return config_node
         else:
-            return config_node_value
+            if(key in config_node):
+                if(is_instance(config_node[key], dict)):
+                    return Configuration(config_node[key])
+                else:
+                    return config_node[key]
+            else:
+                raise NotFoundError(f"Key '{key}' not found in configuration.")
         
     @classmethod
     def load(cls, config_file_path):
@@ -21,9 +38,13 @@ class Configuration(object):
             return configuration
         
     @classmethod
-    def get(cls):
+    def get(cls, key=None):
         configuration = GlobalStorage.get(f'7fae92dc-fcd4-4a98-8d97-4605189335e8')
+        if(key is not None):
+            return configuration[key]
         return configuration
+
+
 
 
 
--- solution.devfx/devfx_samples/config/configuration.py
+++ solution.devfx/devfx_samples/config/configuration.py
@@ -3,6 +3,10 @@ import devfx.config as cfg
 if __name__ == '__main__':
     cfg.Configuration.load('devfx_samples/config/config.json')
 
-    section1 = cfg.Configuration.get()['section1']
+    section1 = cfg.Configuration.get('section1')
     print(section1['value1'])
     print(section1['value2'])
+
+    value111 = cfg.Configuration.get('section1:section11:value111')
+    print(value111)
+
